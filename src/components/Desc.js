958a67ce3703311f47118010d0fff4beecf50957
Erkin <erkinture@gmail.com>
2024-03-16 17:47:28 +0100

    version 2 updates

--- src/components/Desc.js
+++ src/components/Desc.js
@@ -1,58 +1,25 @@
-import '../public/styles/desc.css';
-import Box from '@material-ui/core/Box';
+import "../public/styles/desc.css";
+import Box from "@material-ui/core/Box";
 
 const Desc = () => {
   return (
-    <div style={{ width: '100%', marginTop: '150px', marginBottom: '50px' }}>
+    <div style={{ width: "100%", marginTop: "20vh", marginBottom: "50px" }}>
       <Box display="flex" justifyContent="center">
         <div className="headline">
           <h1
             className="sha"
             style={{
-              fontSize: '3rem',
-              letterSpacing: '-1px',
-              color: '#AFAFAF',
+              fontSize: "3rem",
+              letterSpacing: "-1px",
+              color: "#AFAFAF",
             }}
           >
-            Hello, I am Erkin
+            <span
+              className="txt-rotate"
+              data-period={1000}
+              data-rotate='[ "Hello, I am Erkin", "Hallo, ich bin Erkin","Hola, soy Erkin","我是Erkin","Ciao, sono Erkin","Bonjour, je suis Erkin","Hej, jag är Erkin","Merhaba, ben Erkin"]'
+            />
           </h1>
-
-          <div className="middle" style={{ marginBottom: '65px' }}>
-            <span className="text-type-animation">
-              <h3
-                className="sha"
-                style={{ lineHeight: '1.8', color: '#9a9a9a' }}
-              >
-                <br />
-              </h3>
-              <h2
-                className="sha"
-                style={{
-                  fontSize: '2rem',
-                  letterSpacing: '-1px',
-                  color: '#cdcdcd',
-                }}
-              >
-                I'm into <br></br>{' '}
-                <span
-                  className="txt-rotate"
-                  data-period={1500}
-                  data-rotate='[ "designing software architecture", "developing full stack applications","free diving 🤿"]'
-                />
-                <br></br>
-              </h2>
-              <div
-                className="sha jumping-button"
-                style={{
-                  fontSize: '2.5rem',
-                  color: '#7a7a7a',
-                  marginTop: '50px',
-                }}
-              >
-                <i className="fa fa-chevron-down" aria-hidden="true"></i>
-              </div>
-            </span>
-          </div>
         </div>
       </Box>
     </div>
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,84 +1,55 @@
-import { Fragment } from 'react';
-import Link from '@material-ui/core/Link';
-import WhoIam from './WhoIam';
-import Experiences from './Experiences';
-import Projects from './Projects';
-import configtheworldblog from '../public/img/configtheworldblog.png';
-import me from '../public/img/erkin.jpg';
-import github from '../public/img/githubpp.png';
-import '../public/styles/pulseball.css';
-import '../public/styles/slide.css';
-import Pulse from './Pulse';
-import Desc from './Desc';
-import Stats from './Stats';
-import ContactForm from './ContactForm';
-import SocialMediaButtons from './SocialMediaButtons';
+import { Fragment } from "react";
+import WhoIam from "./WhoIam";
+import Experiences from "./Experiences";
+import Projects from "./Projects";
+import me from "../public/img/erkin.jpg";
+import "../public/styles/pulseball.css";
+import "../public/styles/slide.css";
+import Pulse from "./Pulse";
+import Desc from "./Desc";
+import Stats from "./Stats";
+import ContactForm from "./ContactForm";
+import SocialMediaButtons from "./SocialMediaButtons";
+import React from "react";
+import Wave from "react-wavify";
 
 const MainPage = () => {
   return (
     <Fragment>
       <header className="header-design">
         <div className="listar-map-button">
-          <Link href="http://configtheworld.herokuapp.com/">
-            <img
-              className="imagehover"
-              src={configtheworldblog}
-              height="90px"
-              width="90px"
-              alt="start4ups"
-              style={{
-                borderRadius: '50%',
-                position: 'relative',
-                marginTop: '100px',
-                boxShadow: ' 0 0 0 3px #8c52ff',
-                zIndex: '1',
-                marginRight: '-8px',
-                backgroundColor: '#313131',
-              }}
-            />
-          </Link>
-          <Link to="/">
-            <img
-              className=""
-              src={me}
-              height="100px"
-              width="100px"
-              alt="me"
-              style={{
-                borderRadius: '50%',
-                marginTop: '100px',
-                position: 'relative',
-                boxShadow: ' 0 0 0 3px #50514F',
-                zIndex: '2',
-                filter: 'grayscale(30%)',
-                objectFit: 'cover',
-              }}
-            />
-          </Link>
-          <Link href="https://github.com/configtheworld">
-            <img
-              className="imagehover"
-              src={github}
-              height="90px"
-              width="90px"
-              alt="github"
-              style={{
-                borderRadius: '50%',
-                position: 'relative',
-                marginTop: '100px',
-                boxShadow: ' 0 0 0 3px #30a14e',
-                zIndex: '1',
-                marginLeft: '-8px',
-              }}
-            />
-          </Link>
+          <img
+            className=""
+            src={me}
+            height="150px"
+            width="150px"
+            alt="me"
+            style={{
+              borderRadius: "50%",
+              marginTop: "100px",
+              position: "relative",
+              boxShadow: " 0 0 0 6px #50514F",
+              zIndex: "2",
+              filter: "grayscale(30%)",
+              objectFit: "cover",
+            }}
+          />
         </div>
-        <div className="footer-wave" />
+        <Wave
+          fill="#343434"
+          paused={false}
+          style={{ display: "flex" }}
+          options={{
+            height: 20,
+            amplitude: 20,
+            speed: 0.15,
+            points: 7,
+          }}
+        />
       </header>
 
       <Desc />
       <Pulse />
-      <Stats />
       <div className="pset">
         <WhoIam xs={12} />
         <Experiences xs={6} />
